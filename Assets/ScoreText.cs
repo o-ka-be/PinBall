1958a387b66d87d51ea316443123140b115ee0a1
o-ka-be <cosmlph.notsu@gmail.com>
2025-06-29 14:10:42 +0900

    ScoreText test
    ・Lesson 5 Chapter 9課題のため、関係しそうな内容をひとまずまとめたScoreTextを追加
    ・ScoreTextの表示位置が右上になるように調整

---
+++ Assets/ScoreText.cs
@@ -0,0 +1,53 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
+
+public class ScoreText : MonoBehaviour
+{
+    public int Score = 0;
+    public int ScoreSmallStar = 5;
+    public int ScoreLargeStar = 20;
+    public int ScoreSmallCloud = 50;
+    public int ScoreLargeCloud = 10;
+
+    //衝突時に呼ばれる関数
+    void OnTriggerEnter(Collider other)
+    {
+        //SmallStarにぶつかった際の得点加算
+        if (tag == "SmallStarTag")
+        {
+            Score += ScoreSmallStar;
+        }
+        else if (tag == "LargeStarTag")
+        {
+            Score += ScoreLargeStar;
+        }
+        else if (tag == "SmallCloudTag")
+        {
+            Score += ScoreSmallCloud;
+        }
+        else if (tag == "LargeCloudTag")
+        {
+            Score += ScoreLargeCloud;
+        }
+
+
+    }
+    //スコアを表示するテキスト
+    private GameObject scoreText;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //scene中のScoreTextオブジェクトを取得
+        this.scoreText = GameObject.Find("ScoreText");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Scoretextに現在のスコアを表示する
+        this.scoreText.GetComponent<Text> ().text = "Score" + Score;
+        
+    }
+}
